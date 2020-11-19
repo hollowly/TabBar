340092dc525c16a97dea229a7db3d26e861e7b2b
hollowly <2499538506@qq.com>
2020-11-19 13:59:00 +0800

    enter

--- build/webpack.prod.conf.js
+++ build/webpack.prod.conf.js
@@ -35,7 +35,7 @@ const webpackConfig = merge(baseWebpackConfig, {
     new UglifyJsPlugin({
       uglifyOptions: {
         compress: {
-          warnings: false
+          warnings: true
         }
       },
       sourceMap: config.build.productionSourceMap,
--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-<div id='#app'>
+<div id='app'>
 	<tab-bar-main></tab-bar-main>
 </div>
 </template>
